0f262071bba1c7f55ebcaf23976f2f494e3864fc
Gusta101 <gu_gui14@hotmail.com>
2024-02-05 14:25:09 -0300

    Apresentação de dados por banco de dados

--- core/views.py
+++ core/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
+from .models import Produto
 
 
 def index(request):
+    produtos = Produto.objects.all()
     context = {
         "curso": "Curso de Python com foco em Django para Fullstack",
-        "titulo2": "Não sei o que escrever"
+        "titulo2": "Não sei o que escrever",
+        "produtos": produtos,
     }
     return render(request, 'index.html', context)
 
 
 def contato(request):
     return render(request, "contato.html")
+
+
+def produto(request, pk):
+    prod = Produto.objects.get(pk)
+    context = {
+        "produto": prod
+    }
+    return render(request, "produto.html", context)
